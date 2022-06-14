66cc234c1acff3e6816eecbe2fd328e2e07eb368
Jamie Coupe <jamie.coupe@morganstanley.com>
2022-06-14 20:28:59 +0000

    Added python sytax assessment

---
+++ self-assessments/Python-Syntax.py
@@ -0,0 +1,9 @@
+## Jamie Coupe 14/06/2022
+
+if __name__ == __main__: 
+    firstname = input('What is your first name? ")
+    lastname = input('What is your last name? ")
+    phonenumber = input('What is your phone number? ")
+    year_born = input('What year where you born? ")
+                      
+    print(f"Hello {firstname} {lastname}, your phone number is {phonenumber} and you where born in {year_born}")
